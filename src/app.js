59941d9470e80a93237a21951a4b800764beeb9a
Benedetta Arinci <benedetta.arinci@gmail.com>
2024-04-19 00:08:03 +0200

    prettier setup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
-const template = <h1>This is JSX</h1>
+const template = (
+  <div>
+    <h1>This is a first template</h1>
+  </div>
+)
+
+const template2 = (
+  <div>
+    <h1>BeneArinci</h1>
+    <p>Age: 34</p>
+    <p>City: 34</p>
+  </div>
+)
 
 const appRoot = document.getElementById('app')
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(template2, appRoot)
