0621c90bffd9fc284b2b987014438e24272010ea
Benedetta Arinci <benedetta.arinci@gmail.com>
2024-04-21 17:44:30 +0200

    App functionality V1

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ const app = {
 const onFormSubmit = (e) => {
   e.preventDefault()
   const option = e.target.elements.option.value
-  console.log(option)
 
   if (option) {
     app.options.push(option)
@@ -21,18 +20,32 @@ const onRemoveAllOptions = () => {
   renderForm()
 }
 
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length)
+  const option = app.options[randomNum]
+  alert(option)
+}
+
 const appRoot = document.getElementById('app')
 
 const renderForm = () => {
   const template = (
     <div>
       <h1>{app.title}</h1>
-      <p>Items in the list: {app.options.length}</p>
-      <button onClick={onRemoveAllOptions}>Remove options</button>
+      <p>Here are your options</p>
+      <ol>
+        {app.options.map((option, i) => (
+          <li key={i}>{option}</li>
+        ))}
+      </ol>
       <form onSubmit={onFormSubmit}>
         <input type="text" name="option" />
         <button>Add Option</button>
       </form>
+      <button disabled={!app.options.length} onClick={onMakeDecision}>
+        What should I do?
+      </button>
+      <button onClick={onRemoveAllOptions}>Remove options</button>
     </div>
   )
 
