a063a732dc58a0464c6e8d8bf75ac5b8e6e4f55d
Benedetta Arinci <benedetta.arinci@gmail.com>
2024-04-28 22:49:08 +0200

    React-hot-toast

--- src/components/AddOptionForm.js
+++ src/components/AddOptionForm.js
@@ -1,28 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import toast, { Toaster } from 'react-hot-toast'
 
 export const AddOptionForm = ({ addOption, options }) => {
-  const [error, setError] = useState(undefined)
+  const [isError, setIsError] = useState(false)
+  const notifyError = () => toast.error('Option already available in the list')
   const handleAddOption = (e) => {
     e.preventDefault()
     const option = e.target.elements.option.value.trim()
     if (!!option) {
       if (options.includes(option)) {
-        setError('Option already available in the list')
+        setIsError(true)
         return
       }
       addOption(option)
       e.target.elements.option.value = ''
-      setError(undefined)
     }
   }
 
+  useEffect(() => {
+    if (!!isError) {
+      notifyError()
+      setIsError(false)
+    }
+  })
+
   return (
     <>
-      {error && <p>{error}</p>}
       <form onSubmit={handleAddOption}>
         <input type="text" name="option" />
         <button className="button">Add Option</button>
       </form>
+      <Toaster
+        position="bottom-center"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: '#1c1c1c',
+            color: '#fff',
+            border: '1px solid',
+            borderColor: '#f8ed62',
+          },
+        }}
+      />
     </>
   )
 }
