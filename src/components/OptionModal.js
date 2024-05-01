4708e926562f3f1fe3d0613e73d5c16af380f0b5
Benedetta Arinci <benedetta.arinci@gmail.com>
2024-05-01 23:44:10 +0200

    Modal styling

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -5,10 +5,14 @@ const OptionModal = ({ selectedOption, onClearSelection }) => (
     isOpen={!!selectedOption}
     contentLabel="Selected Option"
     appElement={document.getElementById('app')}
+    closeTimeoutMS={200}
+    className={'modal'}
   >
-    <h3>Selected Option</h3>
-    {selectedOption && <p>{selectedOption}</p>}
-    <button onClick={onClearSelection}>Good luck!</button>
+    <h3 className="modal__title">Selected Option</h3>
+    {selectedOption && <p className="modal__body">{selectedOption}</p>}
+    <button className="button button--active" onClick={onClearSelection}>
+      Good luck!
+    </button>
   </Modal>
 )
 
